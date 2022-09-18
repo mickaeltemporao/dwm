12549ff4708e8981a55216fd20ed3a6cc9d7abb3
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2022-09-18 12:13:16 +0200

    added passmenu and change gap size

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 3;  /* border pixel of windows */
-static const unsigned int snap     = 32;  /* snap pixel */
+static const unsigned int borderpx = 2.5;  /* border pixel of windows */
+static const unsigned int snap     = 20;  /* snap pixel */
 static const unsigned int gappih   = 10;  /* horiz inner gap between windows */
 static const unsigned int gappiv   = 10;  /* vert inner gap between windows */
 static const unsigned int gappoh   = 10;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov   = 3;  /* vert outer gap between windows and screen edge */
+static const unsigned int gappov   = 10;  /* vert outer gap between windows and screen edge */
 static const int smartgaps         = 0;  /* 1 means no outer gap when there is only one window */
 static const int showbar           = 1;  /* 0 means no bar */
 static const int topbar            = 0;  /* 0 means bottom bar */
@@ -60,6 +60,7 @@ static const Rule rules[] = {
 static char dmenumon[2]       = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *dmenucmd[] = { "dmenu_run" };
+static const char *passcmd[]  = { "passmenu", NULL};
 static const char *scrncmd[]  = { "printscreen",  NULL};
 static const char *areacmd[]  = { "selectscreen",  NULL};
 static const char *brupcmd[]  = { "light", "-A", "1", NULL };
@@ -75,6 +76,7 @@ static const char *ibrwcmd[]  = { "brave", "--incognito", NULL };
 static Key keys[]             = {
     /* modifier         key                       function        argument */
     { MODKEY,           XK_p,                     spawn,          {.v = dmenucmd } },
+    { MODKEY|ShiftMask, XK_p,                     spawn,          {.v = passcmd } },
     { MODKEY|ShiftMask, XK_Return,                spawn,          {.v = termcmd } },
     { MODKEY,           XK_e,                     spawn,          {.v = explcmd } },
     { MODKEY,           XK_w,                     spawn,          {.v = brwscmd } },
